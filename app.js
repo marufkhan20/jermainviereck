102b89e56c47b7dc5c7b9cf060a2bf05fb716841
developmentd420 <development.d420@gmail.com>
2024-08-18 23:38:32 +0600

    Fix fs issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 // Set EJS as the templating engine
 app.set("view engine", "ejs");
